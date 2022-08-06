05ded903a137ae4c8840b01becb421138778319d
DeckardKain <philipmullins@hotmail.ca>
2022-08-06 11:14:32 -0600

    Added BlazorUI  - Created Login / Logout Feature.

---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,39 @@
+﻿using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Providers;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+        public AuthenticationService(IClient httpClient, ILocalStorageService localStorage, AuthenticationStateProvider authenticationStateProvider)
+        {
+            this._httpClient = httpClient;
+            this._localStorage = localStorage;
+            this._authenticationStateProvider = authenticationStateProvider;
+        }
+        public async Task<bool> AuthenticateAsync(LoginUserDTO loginModel)
+        {
+            var response = await _httpClient.LoginAsync(loginModel);
+
+            //Store Token
+            await _localStorage.SetItemAsync("accessToken", response.Token);
+
+            //Change auth state of app
+            await ((ApiAuthenticationStateProvider)_authenticationStateProvider).LoggedIn();
+
+            return true;
+        }
+
+        public async Task Logout()
+        {
+            await ((ApiAuthenticationStateProvider)_authenticationStateProvider).LoggedOut();
+        }
+    }
+}
+
